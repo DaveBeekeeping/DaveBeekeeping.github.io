ffeb431fae05fe30de2ba9f15486e56f9577b0c0
Xander <XanderG2@users.noreply.github.com>
2023-09-09 17:01:23 +0100

    change again

--- index.js
+++
@@ -1,8 +0,0 @@
-var imgs = document.getElementsByTagName("img");
-for (var i = 0; i < imgs.length; i++ ) {
-    img = imgs[i];
-    realWidth = img.naturalWidth;
-    realHeight = img.naturalHeight;
-    img.style.width = realWidth / 20;
-    img.style.height = realHeight / 20;
-}
