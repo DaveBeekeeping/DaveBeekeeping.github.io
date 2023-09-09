04c110c48b9aa288043f533bd7a56b15728db983
Xander <XanderG2@users.noreply.github.com>
2023-09-09 16:57:24 +0100

    g

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ for (var i = 0; i < imgs.length; i++ ) {
     img = imgs[i];
     realWidth = img.naturalWidth;
     realHeight = img.naturalHeight;
-    img.style.width = realWidth / 3;
-    img.style.height = realHeight / 3;
+    img.style.width = realWidth / 20;
+    img.style.height = realHeight / 20;
 }
